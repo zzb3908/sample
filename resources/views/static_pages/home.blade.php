bb1c9a19554e8897dec89c26599d9ec367ccac79
nagi <594708461@qq.com>
2018-09-28 08:16:16 +0000

    -update database

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
     <div class="jumbotron">
-        <h1>八岁你个基佬！你个死GAY！！！</h1>
-        {{--<p class="lead">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
             你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">Laravel 入门教程</a> 的项目主页。
         </p>
         <p>
             一切，将从这里开始。
         </p>
-        <p>--}}
+        <p>
             <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
         </p>
     </div>
