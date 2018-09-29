37980d2dcddad9cbf65da1699d5102bd97652431
nagi <594708461@qq.com>
2018-09-29 06:31:12 +0000

    finish user edit...

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,13 @@ use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -21,7 +28,7 @@ class SessionsController extends Controller
 
         if(Auth::attempt($credentials,$request->has('remember'))){
             session()->flash('success','欢迎回来！');
-            return redirect()->route('users.show',[Auth::user()]);
+            return redirect()->intended(route('users.show',[Auth::user()]));
         }else{
             session()->flash('danger','您输入的账号或密码错误');
             return redirect()->back()->withInput();
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        \Illuminate\Database\Eloquent\Model::unguard();
+        $this->call(UsersTableSeeder::class);
+        \Illuminate\Database\Eloquent\Model::reguard();
     }
 }
